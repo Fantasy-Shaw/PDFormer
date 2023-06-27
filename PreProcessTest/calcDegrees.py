f5c0b71a8237e4c819d3d076c0d224f823db2a36
Xu Xiao <609465835@qq.com>
2023-06-27 11:04:54 +0800

    节点处入度和道路重要性权重统计

--- PreProcessTest/calcDegrees.py
+++ PreProcessTest/calcDegrees.py
@@ -10,7 +10,8 @@ class DegreeStatistics:
     def __init__(self):
         self.fullGeoFile: str = os.path.join("..", r"raw_data\PeMS07\PeMS07.geo")
         self.fullRelFile: str = os.path.join("..", r"raw_data\PeMS07\PeMS07.rel")
-        self.outPut: str = os.path.join("..", r"raw_data\PeMS07\PeMS07_DegStat.csv")
+        self.nodeOutPut: str = os.path.join("..", r"raw_data\PeMS07\PeMS07_DegStat.csv")
+        self.roadOutPut: str = os.path.join("..", r"raw_data\PeMS07\PeMS07_RoadStat.csv")
         self.fullGeo: pd.DataFrame = pd.read_csv(self.fullGeoFile, index_col="geo_id")
         self.fullRel: pd.DataFrame = pd.read_csv(self.fullRelFile, index_col="rel_id")
         # 入度
@@ -31,13 +32,20 @@ class DegreeStatistics:
         self.nodeDegs = pd.concat([pd.DataFrame([self.degMap]), pd.DataFrame([self.posDegMap]),
                                    pd.DataFrame([self.negDegMap])], sort=False).T
         self.nodeDegs.columns = ['deg', 'pos_deg', 'neg_deg']
-        self.nodeDegs.to_csv(self.outPut)
+        self.nodeDegs.insert(self.nodeDegs.shape[1], 'geo_id', self.nodeDegs.index)
+        self.nodeDegs = self.nodeDegs[['geo_id', 'deg', 'pos_deg', 'neg_deg']]
+        self.nodeDegs.to_csv(self.nodeOutPut, index=False)
         for src, dst in zip(self.fullRel.origin_id, self.fullRel.destination_id):
             __w = self.nodeDegs.deg[src] + self.nodeDegs.deg[dst]
             try:
                 self.roadStatusStat[__w] += 1
             except KeyError:
                 self.roadStatusStat[__w] = 1
+        self.roadStatusStat = pd.DataFrame([self.roadStatusStat]).T
+        self.roadStatusStat.columns = ['nums']
+        self.roadStatusStat.insert(self.roadStatusStat.shape[1], 'weight', self.roadStatusStat.index)
+        self.roadStatusStat = self.roadStatusStat[['weight', 'nums']]
+        self.roadStatusStat.to_csv(self.roadOutPut, index=False)
         print(self.roadStatusStat)
 
 
